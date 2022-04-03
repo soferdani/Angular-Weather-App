2bd0bca738ba29f3ebaf0e4efb5af3051452700d
soferdani <soferdani@gmail.com>
2022-04-03 12:54:55 +0300

    add some layout

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -11,6 +11,21 @@ export class CurrentWeatherComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  value = 'Clear me';
+
+  handelClick() {
+    // console.log("click");
+  }
+
+  handelInput(event: any) {
+    // console.log(event.value);
+  }
+
+  forecast = [
+    { day: 'Monday', temp: 22 },
+    { day: 'Tuesday', temp: 23 },
+    { day: 'Wednesday', temp: 24 },
+    { day: 'Thursday', temp: 25 },
+    { day: 'Friday', temp: 26 },
+  ]
 
 }
